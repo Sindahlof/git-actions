69d10acf542a6751ea596309a8438480857fbb01
Sindahlof <90039730+Sindahlof@users.noreply.github.com>
2023-08-16 13:21:27 +0200

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
+echo "$docker_password" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
+docker push "ghcr.io/$docker_username_lower/micronaut-app:1.0-${GIT_COMMIT::8}"
+docker push "ghcr.io/$docker_username_lower/micronaut-app:latest" &
+wait
